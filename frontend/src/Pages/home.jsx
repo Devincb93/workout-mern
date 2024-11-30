d4c1502ccd495dfa025a78d6ecbec177a8d8947e
Devin <devincb93@gmail.com>
2024-11-30 12:04:51 -0800

    Began integrating backend to frontend. correct URL for backend althought backend is throwing back error unable to fetch. Must Troubleshoot

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState([])
+
+    useEffect(() => {
+
+        
+
+        const fetchWorkouts = async ()=> {
+           const resp = await fetch('http://localhost:4000/api/workouts') 
+           const json = await resp.json()
+
+           if (resp.ok) {
+            setWorkouts(json)
+            console.log(json)
+           }
+        }
+        fetchWorkouts()
+    },[])
     return (
         <div className="Home">
             <h2>Home</h2>
